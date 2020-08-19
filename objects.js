214fd330c23bbd07bf1a81add77360221e957d17
Michael Rakitzis <michael.rakitzis@gmail.com>
2020-08-19 16:16:51 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,7 +1,12 @@
-var playlist = (artist , song) {
-  blink 182 : "All The Small Things",
-              "Whats My Age Again";
-
-  Sum 41 : "Fat Lip",
+var playlist = {
+182 : {
+  artist: "blink 182",
+  tracks: "All The Small Things",
+          "Whats My Age Again";
+}
+41 : {
+  artist : "Sum 41",
+  tracks : "Fat Lip",
             "In Too Deep";
- }
+}
+}
