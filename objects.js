d483e3aa4c5590b6bb6c6150716bb7adf27a131f
Michael Rakitzis <michael.rakitzis@gmail.com>
2020-08-19 16:30:37 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {
-  playlist = ["alt"]
-  artistName = ["Sum 41"]
+  playlist = "alt"
+  artistName = "Sum 41"
   songTitle : ["Fat Lip", "In Too Deep", "Still Waiting"]
 }
 function updatePlaylist( playlist, artistName, songTitle){
